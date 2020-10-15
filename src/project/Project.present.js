eff234b581cb1b6b8564e88928d527e5cc79064e
Lorenzo Cavazzi <lorenzo.cavazzi.tech@gmail.com>
2020-10-15 18:55:20 +0200

    TEMP

--- src/project/Project.present.js
+++ src/project/Project.present.js
@@ -29,7 +29,8 @@ import React, { Component, Fragment, useState, useEffect } from "react";
 import { Link, Route, Switch } from "react-router-dom";
 import {
   Container, Row, Col, Alert, DropdownItem, Table, Nav, NavItem, Button, ButtonGroup, Badge, Spinner,
-  Card, CardBody, CardHeader, Form, FormGroup, FormText, Label, Input, UncontrolledTooltip, ListGroupItem
+  Card, CardBody, CardHeader, Form, FormGroup, FormText, Label, Input, UncontrolledTooltip, ListGroupItem,
+  UncontrolledCollapse
 } from "reactstrap";
 import qs from "query-string";
 
@@ -44,7 +45,7 @@ import { faGitlab } from "@fortawesome/free-brands-svg-icons";
 
 import {
   Clipboard, ExternalLink, Loader, RenkuNavLink, TimeCaption, RefreshButton, Pagination,
-  ButtonWithMenu, InfoAlert, SuccessAlert, WarnAlert, ErrorAlert, GoBackButton, RenkuMarkdown
+  ButtonWithMenu, InfoAlert, SuccessAlert, WarnAlert, ErrorAlert, GoBackButton, RenkuMarkdown,
 } from "../utils/UIComponents";
 import { SpecialPropVal } from "../model/Model";
 import { ProjectTags, ProjectTagList } from "./shared";
@@ -140,8 +141,10 @@ class MergeRequestSuggestions extends Component {
 /**
  * If appropriate, show a warning banner that the project has not been integration with the knowledge graph.
  */
+// ! TODO HERE
 class KnowledgeGraphIntegrationWarningBanner extends Component {
   render() {
+    //console.log(this.props.webhook)
     if (this.props.webhook.stop) return null;
 
     const status = this.props.webhook.status;
@@ -155,7 +158,6 @@ class KnowledgeGraphIntegrationWarningBanner extends Component {
           isPrivate={isPrivate} />
       );
     }
-    // I would keep this error for safety, even if this error *should* not happen
     const error = webhookError(status);
     if (error) {
       return (
@@ -165,7 +167,16 @@ class KnowledgeGraphIntegrationWarningBanner extends Component {
         </WarnAlert>
       );
     }
-    return null;
+    // ! TODO - FROM HERE
+    --
+    console.log(this.props.webhook.possible)
+    if (this.props.webhook.possible === null)
+      return (<Loader />);
+    return (
+      <Alert color="success">
+        Knowledge Graph integration is active.
+      </Alert>
+    );
   }
 }
 
@@ -303,11 +314,13 @@ class ProjectViewHeaderOverview extends Component {
       <Container fluid>
         <Row>
           <Col xs={12} md>
-            <h3><ProjectMigrationIcon
-              history={this.props.history}
-              overviewVersionUrl={this.props.overviewVersionUrl}
-              migration_required={this.props.migration.migration_required}
-            />{core.title} <ProjectVisibilityLabel visibilityLevel={this.props.visibility.level} /></h3>
+            <h3>
+              <ProjectMigrationIcon
+                history={this.props.history}
+                overviewVersionUrl={this.props.overviewVersionUrl}
+                migration_required={this.props.migration.migration_required}
+              />{core.title} <ProjectVisibilityLabel visibilityLevel={this.props.visibility.level} />
+            </h3>
             <p>
               <span>{this.props.core.path_with_namespace}{forkedFrom}</span> <br />
             </p>
@@ -349,6 +362,7 @@ class ProjectViewHeaderOverview extends Component {
   }
 }
 
+// here
 class ProjectViewHeader extends Component {
 
   render() {
@@ -512,124 +526,136 @@ class ProjectViewStats extends Component {
 
 class ProjectViewVersion extends Component {
   render() {
-    const loading = isRequestPending(this.props, "readme");
-    const migration_required = this.props.migration.migration_required;
-    const project_supported = this.props.migration.project_supported;
-    const migration_status = this.props.migration.migration_status;
-    const check_error = this.props.migration.check_error;
-    const migration_error = this.props.migration.migration_error;
+    const {
+      migration_required, project_supported, migration_status, check_error, migration_error
+    } = this.props.migration;
+    const loading = isRequestPending(this.props, "readme") || migration_required === null;
     const maintainer = this.props.visibility.accessLevel >= ACCESS_LEVELS.MAINTAINER;
 
-    if (loading || (migration_required === null && migration_error === null))
-      return <Loader />;
-
-    return <Card key="storage-stats" className="border-0">
-      <CardHeader>Renku Version</CardHeader>
-      <CardBody>
-        <Col md={10} sm={12}>
-          <Row>
-            <Col>
-              { migration_required ?
-                <Alert color="warning">
-                  <FontAwesomeIcon icon={faExclamationTriangle} /> A new
-                  version of <strong>renku</strong> is availabe.
-                  <br /><br />
-                  This project is currently using an older version of renku.
-                  A migration is necessary to make changes to datasets and is recommended for all projects.
-                  <br /><br />
-                  {
-                    migration_status === MigrationStatus.ERROR ?
-                      <div>
-                        There was an error while trying to upgrade your project. Please try again,
-                        if the problem persists you should contact the development team on&nbsp;
-                        <a href="https://gitter.im/SwissDataScienceCenter/renku"
-                          target="_blank" rel="noreferrer noopener">Gitter</a> or create an issue in&nbsp;
-                        <a href="https://github.com/SwissDataScienceCenter/renku/issues"
-                          target="_blank" rel="noreferrer noopener">GitHub</a>.
-                        <br /><br />
-                        <strong>Error: </strong>{migration_error}
-                      </div>
-                      : null
+    let body = null;
+    if (loading) {
+      body = (<Loader />);
+    }
+    else {
+      // print the error if any
+      if (check_error || migration_status === MigrationStatus.ERROR) {
+        const error = check_error ?
+          check_error :
+          migration_status;
+        body = (
+          <Alert color="danger">
+            <p>
+              Error while { check_error ? "checking" : "updating" } the version. Please reload the page to try again.
+              If the problem persistsy you should contact the development team on&nbsp;
+              <a href="https://gitter.im/SwissDataScienceCenter/renku"
+                target="_blank" rel="noreferrer noopener">Gitter</a> or create an issue in&nbsp;
+              <a href="https://github.com/SwissDataScienceCenter/renku/issues"
+                target="_blank" rel="noreferrer noopener">GitHub</a>.
+            </p>
+            <div><strong>Error Message</strong><pre>{error}</pre></div>
+          </Alert>
+        );
+      }
+      // migration required
+      else if (migration_required) {
+        let updateSection = null;
+        const updateInstruction = (
+          <Fragment>
+            You can launch
+            an <Link to={this.props.launchNotebookUrl}>interactive environment</Link> and follow the
+            {/* eslint-disable-next-line max-len */}
+            &nbsp;<a href="https://renku.readthedocs.io/en/latest/user/upgrading_renku.html#upgrading-your-image-to-use-the-latest-renku-cli-version">
+              instructions for upgrading</a>.
+            When finished, you will need to run <code>renku migrate</code>.
+          </Fragment>
+        );
+        if (maintainer) {
+          if (project_supported) {
+            updateSection = (
+              <Fragment>
+                <Button
+                  color="warning"
+                  disabled={migration_status === MigrationStatus.MIGRATING}
+                  onClick={this.props.onMigrateProject}
+                >
+                  {migration_status === MigrationStatus.MIGRATING ?
+                    <span><Spinner size="sm" /> Updating...</span> : "Update"
                   }
-                  {
-                    project_supported ?
-                      maintainer ?
-                        <div>
-                          <Col align="right">
-                            <Button
-                              color="warning"
-                              disabled={migration_status === MigrationStatus.MIGRATING}
-                              onClick={this.props.onMigrateProject}>
-                              {migration_status === MigrationStatus.MIGRATING ?
-                                <span>
-                                  <Spinner size="sm" /> Updating...
-                                </span>
-                                :
-                                "Update"
-                              }
-                            </Button>
-                          </Col>
-                          <br /><br />
-                          Or, if you prefer, you can update the project manually by launching&nbsp;
-                          an <Link to={this.props.launchNotebookUrl}>interactive environment</Link> and follow the
-                          {/* eslint-disable-next-line max-len */}
-                          <a href="https://renku.readthedocs.io/en/latest/user/upgrading_renku.html#upgrading-your-image-to-use-the-latest-renku-cli-version">
-                              &nbsp;instructions for upgrading</a>.
-                          When finished, you will need to run <i>renku migrate</i>.&nbsp;
-                          If you encounter any problems or have any questions,&nbsp;
-                          please contact the development team on&nbsp;
-                          <a href="https://gitter.im/SwissDataScienceCenter/renku"
-                            target="_blank" rel="noreferrer noopener">Gitter</a> or create an issue in&nbsp;
-                          <a href="https://github.com/SwissDataScienceCenter/renku/issues"
-                            target="_blank" rel="noreferrer noopener">GitHub</a>.
+                </Button>
+                <Button color="link" id="btn_instructions"><i>Do you prefer manual instructions?</i></Button>
+                <UncontrolledCollapse toggler="#btn_instructions">
+                  <br />
+                  <p>{updateInstruction}</p>
+                </UncontrolledCollapse>
+              </Fragment>
+            );
+          }
+          else {
+            updateSection = (
+              <p>
+                <strong>Updating this project automatically is not possible.</strong>
+                <br /> {updateInstruction}
+              </p>
+            );
+          }
+        }
+        else {
+          updateSection = (
+            <p>
+              <strong>You do not have the required permissions to upgrade this project.</strong>
+              &nbsp;You can <ExternalLink role="text" size="sm"
+                title="ask a project maintainer" url={`${this.props.externalUrl}/project_members`} /> to
+              do that for you.
+            </p>
+          );
+        }
+        body = (
+          <Alert color="warning">
+            <p>
+              <FontAwesomeIcon icon={faExclamationTriangle} /> A new version of <strong>renku</strong> is available.
+              The project needs to be migrated to keep working.
+            </p>
+            {updateSection}
+          </Alert>
+        );
+      }
+      // migration not needed
+      else {
+        body = (<Alert color="success"><FontAwesomeIcon icon={faCheck} /> The current version is up to date.</Alert>);
+      }
+    }
 
-                        </div>
-                        : <strong>You do not have sufficient rights to upgrade this project, but
-                          a project maintainer can do this. <ExternalLink role="text" size="sm"
-                          title="Contact a maintainer" url={`${this.props.externalUrl}/project_members`} />
-                          .</strong>
-                      : <span>
-                        <strong>This project&apos;s renku version cannot be upgraded automatically</strong>.
-                        <br/><br/>
-                        You can launch an <Link to={this.props.launchNotebookUrl}>
-                          interactive environment</Link> and follow the&nbsp;
-                        {/* eslint-disable-next-line max-len */}
-                        <a href="https://renku.readthedocs.io/en/latest/user/upgrading_renku.html#upgrading-your-image-to-use-the-latest-renku-cli-version">
-                          &nbsp;instructions for upgrading</a>.&nbsp;
-                        When finished, you will need to run <i>renku migrate</i>.&nbsp;
-                        If you encounter any problems or have any questions,&nbsp;
-                        please contact the development team on&nbsp;
-                        <a href="https://gitter.im/SwissDataScienceCenter/renku"
-                          target="_blank" rel="noreferrer noopener">Gitter</a> or create an issue in&nbsp;
-                        <a href="https://github.com/SwissDataScienceCenter/renku/issues"
-                          target="_blank" rel="noreferrer noopener">GitHub</a>.
-                      </span>
-                  }
-                </Alert>
-                :
-                check_error !== undefined ?
-                  <Alert color="danger">
-                    There was an error while performing the migration check, please reload the page and try again.
-                    If the problem persists you should contact the development team on&nbsp;
-                    <a href="https://gitter.im/SwissDataScienceCenter/renku"
-                      target="_blank" rel="noreferrer noopener">Gitter</a> or create an issue in&nbsp;
-                    <a href="https://github.com/SwissDataScienceCenter/renku/issues"
-                      target="_blank" rel="noreferrer noopener">GitHub</a>.
-                    <br></br>
-                    <br></br>
-                    <strong>Error Message: </strong> {check_error}
-                  </Alert>
-                  : migration_required === false ?
-                    <Alert color="success">
-                      <FontAwesomeIcon icon={faCheck} /> The current renku version is compatible with RenkuLab.
-                    </Alert>
-                    : <Loader />
-              }
-            </Col>
-          </Row>
-        </Col>
-      </CardBody>
-    </Card>;
+    return (
+      <Card className="border-0">
+        <CardHeader>Renku Version</CardHeader>
+        <CardBody>
+          <Row><Col>{body}</Col></Row>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+class ProjectViewKG extends Component {
+  render() {
+    const loading = false;
+
+    let body = null;
+    if (loading) {
+      body = (<Loader />);
+    }
+    else {
+      body = (<KnowledgeGraphIntegrationWarningBanner {...this.props} />);
+    }
+
+    return (
+      <Card className="border-0">
+        <CardHeader>Renku Knowledge Graph</CardHeader>
+        <CardBody>
+          <Row><Col>{body}</Col></Row>
+        </CardBody>
+      </Card>
+    );
   }
 }
 
@@ -655,7 +681,7 @@ class ProjectViewOverviewNav extends Component {
           <RenkuNavLink to={`${this.props.overviewCommitsUrl}`} title="Commits" />
         </NavItem>
         <NavItem>
-          <RenkuNavLink to={`${this.props.overviewVersionUrl}`} title="Version" />
+          <RenkuNavLink to={`${this.props.overviewVersionUrl}`} title="Status" />
         </NavItem>
       </Nav>);
   }
@@ -716,7 +742,10 @@ class ProjectViewOverview extends Component {
               }}
             />
             <Route exact path={this.props.overviewVersionUrl} render={props =>
-              <ProjectViewVersion {...this.props} />}
+              <Fragment>
+                <ProjectViewVersion {...this.props} />
+                <ProjectViewKG {...this.props} />
+              </Fragment>}
             />
           </Switch>
         </Col>
