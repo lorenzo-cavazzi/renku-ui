9ab48bc723560fdf022efa5dde6b58cbf9648926
Lorenzo Cavazzi <lorenzo.cavazzi.tech@gmail.com>
2021-02-03 18:24:43 +0100

    temp

--- client/src/project/list/ProjectList.container.js
+++ client/src/project/list/ProjectList.container.js
@@ -16,13 +16,16 @@
  * limitations under the License.
  */
 
-import React, { Component } from "react";
+import React, { Component, useState, useEffect, } from "react";
+import { useParams, useLocation } from "react-router-dom";
+
 import { connect } from "react-redux";
 import qs from "query-string";
 
-import ProjectList from "./ProjectList.present";
+import ProjectListPresent, { ProjectListNew } from "./ProjectList.present";
 import ProjectListModel from "./ProjectList.state";
 import { Loader } from "../../utils/UIComponents";
+import { Url, getSearchParams } from "../../utils/url";
 
 const orderByValuesMap = {
   NAME: "name",
@@ -41,19 +44,363 @@ const searchScopesValuesMap = {
   STARRED: "starred"
 };
 
-const urlMap = {
-  projectsUrl: "/projects",
-  projectsSearchUrl: "/projects/all",
-  projectNewUrl: "/projects/new",
-  starred: "/projects/starred",
-  yourProjects: "/projects"
+const SECTIONS = {
+  own: "own",
+  starred: "starred",
+  all: "all",
 };
+
+const searchInMap = {
+  projects: { value: "projects", text: "Project" },
+  users: { value: "users", text: "User" },
+  groups: { value: "groups", text: "Group" }
+};
+
+const orderByMap = {
+  name: { value: "name", text: "Name" },
+  creationDate: { value: "created_at", text: "Creation date" },
+  updateDate: { value: "last_activity_at", text: "Update date" }
+};
+
+const PROJECT_NEW_URL = Url.get(Url.pages.project.new);
+
+const urlMap = {
+  projectsUrl: Url.get(Url.pages.projects), // TODO: remove?
+  projectsSearchUrl: Url.get(Url.pages.projects.all), // --> all --> /all
+  projectNewUrl: Url.get(Url.pages.project.new),
+  starred: Url.get(Url.pages.projects.starred), // --> starred --> /starred
+  yourProjects: Url.get(Url.pages.projects) // --> your --> null
+};
+
+
+const DEFAULT_PARAMS = {
+  query: "",
+  page: 1,
+  perPage: 6, // TODO: change to 10
+  searchIn: searchInMap.projects.value,
+  orderBy: orderByMap.updateDate.value,
+  ascending: false,
+};
+const CONVERSIONS = {
+  q: "query", currentTab: "section", currentPage: "page", orderSearchAsc: "ascending", usersOrGroup: "targetUser"
+};
+const DEFAULT_PROJECTS = { fetched: null, fetching: null, total: null, pages: null, list: [] };
+const DEFAULT_USERS_GROUPS = { fetched: null, fetching: null, list: [] };
+
+/**
+ * Return section based on current location.
+ *
+ * @param {object} location - React location object.
+ * @returns {string} current section, as defined in the enum SECTIONS.
+ */
+function getSection(location) {
+  let section = SECTIONS.own;
+  if (location && location.pathname) {
+    if (location.pathname.endsWith("/starred"))
+      section = SECTIONS.starred;
+    else if (location.pathname.endsWith("/all"))
+      section = SECTIONS.all;
+  }
+  return section;
+}
+
+/**
+ * Return full URL based on target section and current parameter
+ *
+ * @param {string} target - target section
+ * @param {object} params - parameters object
+ */
+function buildPreciseUrl(target, params) {
+  let page = Url.pages.projects.all;
+  if (target === SECTIONS.own)
+    page = Url.pages.projects.base;
+  else if (target === SECTIONS.starred)
+    page = Url.pages.projects.starred;
+
+  // remove parameters we don't want to propagate
+  // let finalParams = { ...params };
+  // if (finalParams.section)
+  //   delete finalParams.section;
+  // if (target !== SECTIONS.all && finalParams.searchIn)
+  //   delete finalParams.searchIn;
+
+  const url = Url.get(page, params);
+  return url;
+}
+
+/**
+ * Show list of projects, allowing advanced search.
+ *
+ * @param {object} props.location - React location object.
+ * @param {object} props.history - React history object.
+ * @param {object} props.client - client object.
+ * @param {object} props.user - user object.
+ */
+function ProjectList(props) {
+  // Redirect anonymous users when trying to perform an invalid search (manually modified link)
+  // const initSection = getSection(props.location);
+  // const initSearch = getSearchParams();
+  if (!props.user.logged) {
+    const section = getSection(props.location);
+    const searchParams = getSearchParams();
+    // Searching in own or starred projects
+    if (section !== SECTIONS.all) {
+      const newUrl = Url.get(Url.pages.projects.all, searchParams);
+      props.history.push(newUrl);
+    }
+    // filtering per user or group
+    if (searchParams.searchIn !== searchInMap.projects.value) {
+      const newParams = { ...searchParams, searchIn: searchInMap.projects.value };
+      const newUrl = Url.get(Url.pages.projects.all, newParams);
+      props.history.push(newUrl);
+    }
+  }
+
+  // Initial setup
+  const [projects, setProjects] = useState(DEFAULT_PROJECTS);
+  const [users, setUsers] = useState(DEFAULT_USERS_GROUPS);
+  const [targetUser, setTargetUser] = useState(null);
+  const [params, setParams] = useState({
+    ...(getSearchParams(DEFAULT_PARAMS, CONVERSIONS)),
+    section: getSection(props.location),
+  });
+
+  // Monitor location changes and set params
+  useEffect(() => {
+    console.log("Set params - global");
+    const newSection = getSection(props.location);
+    let newSearchParams = getSearchParams(null, CONVERSIONS);
+    // prevent illegal searchIn
+    if (newSection !== SECTIONS.all && newSearchParams.searchIn !== searchInMap.projects.value)
+      newSearchParams.searchIn = searchInMap.projects.value;
+    // console.log({ params, ...newSearchParams, section: newSection })
+    // if (newSearchParams.search != null && typeof newSearchParams.search !== "string")
+    //   newSearchParams.search = newSearchParams.search.toString();
+    setParams(p => ({ ...p, ...newSearchParams, section: newSection }));
+  }, [props.location]);
+
+  // Get new users when params change (searchIn --> user or groups)
+  useEffect(() => {
+    if (params.searchIn === searchInMap.projects.value)
+      return;
+    console.log("Get users - user+groups");
+
+    // Never fetch when filtering for something shorter than 3 chars
+    if (params.query == null || !params.query.toString().length || params.query.toString().length < 3) {
+      setUsers({ ...DEFAULT_USERS_GROUPS, fetching: false, fetched: new Date() });
+      return;
+    }
+
+    // prepare fetching users
+    setUsers(u => ({ ...u, fetched: null, fetching: true }));
+    let queryParams = { search: params.query, per_page: 100 };
+
+    // fetch users when feasible
+    props.client.searchUsersOrGroups(queryParams, params.searchIn).then((response) => {
+      const data = response.data ?
+        response.data :
+        response;
+
+      // Set new target -- mind that targetUser is not currently used
+      let target = params.targetUser ?
+        params.targetUser :
+        null;
+      if (!target && data && data.length) {
+        target = data[0].full_path ?
+          encodeURIComponent(data[0].full_path) :
+          data[0].username;
+      }
+      setTargetUser(target);
+
+      // set users at the end to prevent flickering
+      setUsers({
+        fetching: false,
+        fetched: new Date(),
+        list: data,
+      });
+    });
+
+  }, [params.targetUser, params.query, params.searchIn, props.client]);
+
+  // Get new projects when params change (searchIn --> projects)
+  useEffect(() => {
+    if (params.searchIn !== searchInMap.projects.value)
+      return;
+
+    console.log("Get projects - projects");
+
+    // prepare fetching projects
+    setProjects(p => ({ ...p, fetched: null, fetching: true }));
+    let queryParams = {
+      search: params.query,
+      page: params.page,
+      per_page: params.perPage,
+      order_by: params.orderBy,
+      sort: params.ascending ? "asc" : "desc",
+    };
+    if (params.section === SECTIONS.own)
+      queryParams.membership = true;
+    else if (params.section === SECTIONS.starred)
+      queryParams.starred = true;
+
+    // fetch projects when feasible
+    // // if (props.user.logged || (!props.user.logged && params.section === SECTIONS.all)) {
+    props.client.getProjects(queryParams).then((response) => {
+      setProjects({
+        fetching: false,
+        fetched: new Date(),
+        total: response.pagination.totalItems,
+        pages: response.pagination.totalPages,
+        list: response.data,
+      });
+    });
+    // //}
+  }, [params, props.client]);
+
+  // Get new projects when targetUser change (searchIn --> user or groups)
+  useEffect(() => {
+    if (params.searchIn === searchInMap.projects.value)
+      return;
+
+    console.log("Get projects - user+groups");
+
+    // If no users were found, we already know there won't be any project.
+    if (!targetUser) {
+      setProjects({
+        ...DEFAULT_PROJECTS,
+        fetching: false,
+        fetched: new Date(),
+      });
+      return;
+    }
+
+    // Prepare fetching user or group projects
+    setProjects(p => ({ ...p, fetched: null, fetching: true }));
+    let queryParams = {
+      page: params.page,
+      per_page: params.perPage,
+      order_by: params.orderBy,
+      sort: params.ascending ? "asc" : "desc",
+    };
+    // Fetch user or group projects
+    props.client.getProjectsBy(params.searchIn, targetUser, queryParams)
+      .then((response) => {
+        setProjects({
+          fetching: false,
+          fetched: new Date(),
+          total: response.pagination.totalItems,
+          pages: response.pagination.totalPages,
+          list: response.data,
+        });
+      });
+  }, [props.client, params.searchIn, params.page, params.perPage, params.orderBy, params.ascending, targetUser]);
+
+  // Programmatically set the selected user or group
+  const setTarget = (target) => {
+    if (target === targetUser)
+      return;
+    console.log("setTarget", target);
+    setTargetUser(target);
+  };
+
+  // Programmatically move to page with proper query params to perform a new query
+  const search = (newParams, section) => {
+    // Get all the current params that are not default (apart from section that is not a query param).
+    let modifiedParams = {};
+    for (let [param, value] of Object.entries(params)) {
+      if (param !== "section" && value !== DEFAULT_PARAMS[param])
+        modifiedParams[param] = value;
+    }
+
+    // Use the section to decide the target URL since that is not a query param.
+    const targetSection = section ?
+      section :
+      params.section;
+    let target = Url.pages.projects.all;
+    if (targetSection === SECTIONS.own)
+      target = Url.pages.projects.base;
+    else if (targetSection === SECTIONS.starred)
+      target = Url.pages.projects.starred;
+
+    // Fix illegal searchIn
+    if (targetSection !== SECTIONS.all && modifiedParams.searchIn !== searchInMap.projects.value)
+      modifiedParams.searchIn = searchInMap.projects.value;
+    //   const newUrl = Url.get(Url.pages.projects.all, newParams);
+    //   props.history.push(newUrl);
+    // }
+
+    // Move to the target url.
+    let finalParams = { ...modifiedParams, ...(newParams || {}) };
+    // if (finalParams.search != null && typeof finalParams.search !== "string")
+    //   finalParams.search = finalParams.search.toString();
+    const url = Url.get(target, finalParams);
+    console.log(finalParams, url)
+    props.history.push(url);
+  };
+  // if (!props.user.logged && getSection(props.location) !== SECTIONS.all) {
+  //   const newUrl = Url.get(Url.pages.projects.all, getSearchParams());
+  //   props.history.push(newUrl);
+  // }
+
+  const getPreciseUrl = (section) => {
+    let modifiedParams = {};
+    for (let [param, value] of Object.entries(params)) {
+      if (param !== "section" && value !== DEFAULT_PARAMS[param])
+        modifiedParams[param] = value;
+    }
+    if (section !== SECTIONS.all && modifiedParams.searchIn !== searchInMap.projects.value)
+      modifiedParams.searchIn = searchInMap.projects.value;
+    return buildPreciseUrl(section, modifiedParams);
+  };
+
+  const goToSection = (section) => {
+    //const url = buildSection(section, params);
+    console.log("PUSHING " + section);
+    // props.history.push(url);
+    //search(null, section);
+
+    // Redirect illegal searchIn
+  // if (initSection !== SECTIONS.all && initSearch.searchIn !== searchInMap.projects.value) {
+  //   const newParams = { ...initSearch, searchIn: searchInMap.projects.value };
+  //   const newUrl = Url.get(Url.pages.projects.all, newParams);
+  //   props.history.push(newUrl);
+  // }
+  };
+
+  return (
+    // ! If not logged in, don't show the "your" and "starred" tabs.
+    // ! If the user ends up there by manually entering the url, move him or show message
+    // <h3>New Projects List</h3>
+    <ProjectListNew
+      fetched={projects.fetched}
+      fetching={projects.fetching}
+      getAvatar={id => this.client.getAvatarFromNamespace(id)}
+      getPreciseUrl={getPreciseUrl}
+      logged={props.user ? props.user.logged : false}
+      orderByMap={orderByMap}
+      params={params}
+      projectNew={PROJECT_NEW_URL}
+      projects={projects.list}
+      users={users}
+      search={search}
+      searchInMap={searchInMap}
+      sectionsMap={SECTIONS}
+      setTarget={setTarget}
+      target={targetUser}
+      urlMap={urlMap}
+    />
+  );
+}
+
 
 class List extends Component {
   render() {
     const user = this.props.user; // TODO: change to user
     return user.fetched ?
-      <AvailableUserList {...this.props} /> :
+      [
+        <ProjectList key="new" {...this.props} />,
+        // <AvailableUserList key="old" {...this.props} />
+      ] :
       <div>
         <h1>Projects</h1>
         <Loader />
@@ -288,7 +635,7 @@ class AvailableUserList extends Component {
 
   render() {
     const VisibleProjectList =
-      connect(this.mapStateToProps.bind(this))(ProjectList);
+      connect(this.mapStateToProps.bind(this))(ProjectListPresent);
 
     return <VisibleProjectList
       store={this.model.reduxStore}
@@ -305,4 +652,4 @@ class AvailableUserList extends Component {
 }
 
 export default List;
-export { searchInValuesMap, searchScopesValuesMap, urlMap };
+export { searchInValuesMap, urlMap, ProjectList };
--- client/src/project/list/ProjectList.present.js
+++ client/src/project/list/ProjectList.present.js
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-import React, { Component } from "react";
+import React, { Component, Fragment, useState } from "react";
 import { Link, Route, Switch } from "react-router-dom";
 import {
   Row, Col, Alert, UncontrolledTooltip, Button, Form, InputGroup, FormText, Input, Label, ButtonGroup,
@@ -27,9 +27,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { ProjectAvatar, Loader, Pagination, TimeCaption, RenkuNavLink } from "../../utils/UIComponents";
 import { ProjectTagList } from "../shared";
+import { Url } from "../../utils/url";
 
 import "../Project.css";
 
+// // REMOVABLE
 class ProjectListRow extends Component {
   render() {
     const { projectsUrl, description, compact } = this.props;
@@ -178,6 +180,8 @@ class ProjectSearchForm extends Component {
   }
 }
 
+
+// // REMOVABLE
 class ProjectNavTabs extends Component {
   render() {
     return (
@@ -212,6 +216,8 @@ class ProjectNavTabs extends Component {
 }
 
 
+// // REMOVABLE
+// TODO: but keep the text
 class DisplayEmptyProjects extends Component {
   render() {
     return (
@@ -229,6 +235,8 @@ class DisplayEmptyProjects extends Component {
   }
 }
 
+// // REMOVABLE
+// TODO: but resue the content of DisplayEmptyProjects
 class ProjectsRows extends Component {
   render() {
     if (this.props.forbidden)
@@ -285,6 +293,7 @@ class UsersRow extends Component {
   }
 }
 
+// WIP
 class ProjectsSearch extends Component {
   render() {
     const loading = this.props.loading || false;
@@ -357,6 +366,7 @@ class ProjectsSearch extends Component {
   }
 }
 
+// // REMOVABLE
 class NotFoundInsideProject extends Component {
   render() {
     return <Col key="notFound">
@@ -375,6 +385,8 @@ class NotFoundInsideProject extends Component {
   }
 }
 
+// // REMOVABLE
+// ! but recover the `emptyListText`
 class ProjectList extends Component {
   render() {
     if (!this.props.user.fetched)
@@ -392,7 +404,7 @@ class ProjectList extends Component {
     return [
       <Row key="header">
         <Col className="d-flex mb-2">
-          <h1 className="mr-5">Projects</h1>
+          <h1 className="mr-5">Projects OLD</h1>
           {newProjectButton}
         </Col>
       </Row>,
@@ -437,5 +449,309 @@ class ProjectList extends Component {
   }
 }
 
+function ProjectListRowNew(props) {
+  const {
+    owner, path, path_with_namespace, last_activity_at, description, compact, avatar_url, getAvatar, tag_list
+  } = props;
+  const namespace = props.namespace.path;
+
+  const url = Url.get(Url.pages.project, { namespace, path });
+  const title = (<Link to={url}>{path_with_namespace || "no title"}</Link>);
+
+  let directionModifier = "", marginModifier = "";
+  if (!compact) {
+    directionModifier = " flex-sm-row";
+    marginModifier = " ml-sm-auto";
+  }
+
+  return (
+    <div className="d-flex limit-width pt-2 pb-2 border-top">
+      <div className="d-flex flex-column mt-auto mb-auto">
+        <ProjectAvatar
+          owner={owner}
+          avatar_url={avatar_url}
+          namespace={namespace}
+          getAvatarFromNamespace={getAvatar}
+        />
+      </div>
+      <div className={"d-flex flex-fill flex-column ml-2 mw-0" + directionModifier}>
+        <div className="d-flex flex-column text-truncate">
+          <p className="mt-auto mb-auto text-truncate">
+            <b>{title}</b>
+            <span className="ml-2">
+              <ProjectTagList tagList={tag_list} />
+            </span>
+          </p>
+          {description ? <p className="mt-auto mb-auto text-truncate">{description}</p> : null}
+        </div>
+        <div className={"d-flex flex-shrink-0" + marginModifier}>
+          <p className="mt-auto mb-auto">
+            <TimeCaption caption="Updated" time={last_activity_at} />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProjectListRows(props) {
+  const { projects, getAvatar } = props;
+  // TODO: display custom messages for empty starred and own project (without search content).
+  // TODO: take inspiration from here: DisplayEmptyProjects
+  if (!projects || !projects.length)
+    return (<p>We couldn&apos;t find any project matching the search criteria.</p>);
+
+  const rows = projects.map(project =>
+    <ProjectListRowNew key={project.id} getAvatar={getAvatar} {...project} />
+  );
+  return (rows);
+}
+
+function ProjectListSearch(props) {
+  const { orderByMap, params, search, searchInMap, sectionsMap } = props;
+
+  // input and search
+  const [ userInput, setUserInput ] = useState(params.query.toString());
+  const searchWithValues = (modifiedParams) => {
+    let newParams = modifiedParams || {};
+    if (params.query.toString() !== userInput.toString())
+      newParams.query = userInput.toString();
+    search(newParams);
+
+    // if (params.query.toString() !== userInput.toString())
+    //   search({ query: userInput.toString() });
+    // else
+    //   search();
+  };
+
+  // order by
+  const [dropdownOrderBy, setDropdownOrderBy] = useState(false);
+  const toggleDropdownOrderBy = () => setDropdownOrderBy(!dropdownOrderBy);
+  const currentOrderMapObject = Object.values(orderByMap).find(v => v.value === params.orderBy);
+  const orderByItems = Object.values(orderByMap).map(v => (
+    <DropdownItem key={v.value} value={v.value} onClick={() => { searchWithValues({ orderBy: v.value }); }}>
+      {v.value === currentOrderMapObject.value ? <FontAwesomeIcon icon={faCheck} /> : null} {v.text}
+    </DropdownItem>
+  ));
+
+  // search in
+  const [dropdownSearchIn, setDropdownSearchIn] = useState(false);
+  const toggleDropdownSearchIn = () => setDropdownSearchIn(!dropdownSearchIn);
+  const currentSearchInObject = Object.values(searchInMap).find(v => v.value === params.searchIn);
+  const searchInItems = Object.values(searchInMap).map(v => (
+    <DropdownItem key={v.value} value={v.value} onClick={() => { searchWithValues({ searchIn: v.value }); }}>
+      {v.value === currentSearchInObject.value ? <FontAwesomeIcon icon={faCheck} /> : null} {v.text}
+    </DropdownItem>
+  ));
+
+  // ordering
+  const orderingIcon = params.ascending ?
+    faSortAmountUp :
+    faSortAmountDown;
+
+  const filterSection = params.section === sectionsMap.all ?
+    (
+      <InputGroupButtonDropdown className="input-group-prepend" addonType="append"
+        toggle={toggleDropdownSearchIn} isOpen={dropdownSearchIn} >
+        <DropdownToggle outline caret color="primary" >
+          Filter by: {currentSearchInObject.text}
+        </DropdownToggle>
+        <DropdownMenu>
+          {searchInItems}
+        </DropdownMenu>
+      </InputGroupButtonDropdown>
+    ) :
+    null;
+
+  return (
+    <div className="mb-4">
+      <Form inline onSubmit={e => { e.preventDefault(); searchWithValues(); }}>
+        <InputGroup>
+          <Input name="searchQuery" id="searchQuery"
+            className="border-primary input-group-append" style={{ minWidth: "300px" }}
+            placeholder={"Filter by " + currentSearchInObject.text.toLowerCase()} value={userInput}
+            onChange={e => setUserInput(e.target.value.toString())} />
+          {filterSection}
+          <div className="input-group-append input-group-prepend m-0">
+            <Button outline color="primary" onClick={() => { searchWithValues({ ascending: !params.ascending }); }}>
+              <FontAwesomeIcon icon={orderingIcon} />
+            </Button>
+          </div>
+          <InputGroupButtonDropdown addonType="append"
+            toggle={toggleDropdownOrderBy} isOpen={dropdownOrderBy} >
+            <DropdownToggle outline caret color="primary" >
+              Order by: {currentOrderMapObject.text}
+            </DropdownToggle>
+            <DropdownMenu>
+              {orderByItems}
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+        </InputGroup>
+        &nbsp;
+        <Button color="primary" id="searchButton" onClick={() => searchWithValues()}>Search</Button>
+      </Form>
+      <FormText key="help" color="muted">
+        Leave empty to browse all projects or enter at least 3 characters to filter.
+      </FormText>
+    </div>
+  );
+}
+
+function ProjectListUsersFilter(props) {
+  const { users, setTarget, target } = props;
+
+  let usersList = null;
+  if (users.list && users.list.length) {
+    usersList = users.list.map(u => {
+      const identifier = u.full_path ?
+        encodeURIComponent(u.full_path) :
+        u.username;
+      const active = target === identifier ?
+        true :
+        false;
+      return (
+        <Button key={u.id} className="mb-1 mr-1" color="primary" size="sm" outline
+          onClick={() => { setTarget(identifier); }} active={active}>
+          { u.name}
+          <small className="font-italic d-none d-sm-block">{decodeURIComponent(identifier)}</small>
+        </Button>
+      );
+    });
+  }
+
+  const list = usersList ?
+    (<div className="mb-4">{usersList}</div>) :
+    null;
+
+  return (list);
+}
+
+function ProjectListContent(props) {
+  const {
+    fetched, fetching, getAvatar, orderByMap, params, projects, search, searchInMap, sectionsMap,
+    setTarget, users, target
+  } = props;
+
+  let usersFilter = null;
+  if (params.searchIn !== searchInMap.projects.value) {
+    if (users.fetching)
+      usersFilter = (<Loader />);
+    else if (users.fetched)
+      usersFilter = (<ProjectListUsersFilter users={users} setTarget={setTarget} target={target} />);
+  }
+
+  let content = null;
+  // don't show anything if users are updating, since the content would be outdated
+  if (!users.fetching) {
+    if (fetching)
+      content = (<Loader />);
+    else if (!fetched)
+      content = (<p>Please enter you filtering words in the field above and click on Search to get the result.</p>);
+    else
+      content = (<ProjectListRows projects={projects} getAvatar={getAvatar} />);
+  }
+
+  return (
+    <Row className="mb-4">
+      <Col>
+        <ProjectListSearch
+          orderByMap={orderByMap}
+          params={params}
+          search={search}
+          searchInMap={searchInMap}
+          sectionsMap={sectionsMap}
+        />
+        {usersFilter}
+        {content}
+      </Col>
+    </Row>
+  );
+}
+
+function ProjectListNav(props) {
+  const { getPreciseUrl, sectionsMap, urlMap } = props;
+  console.log(getPreciseUrl)
+  return (
+    <Row>
+      <Col>
+        <Nav pills className="nav-pills-underline mb-4">
+          {/* <NavItem onClick={(e) => { e.preventDefault(); props.goToSection(props.sectionsMap.own); }}> */}
+          {/* <NavItem>
+            <RenkuNavLink
+              to={urlMap.projectsUrl}
+              alternate={urlMap.yourProjects}
+              noSubPath={true}
+              title="Your Projects" />
+          </NavItem>
+          <NavItem>
+            <RenkuNavLink exact={false} to={urlMap.starred} title="Starred Projects" />
+          </NavItem>
+          <NavItem>
+            <RenkuNavLink exact={false} to={urlMap.projectsSearchUrl} title="All Projects" />
+          </NavItem> */}
+          <NavItem>
+            <RenkuNavLink
+              to={getPreciseUrl(sectionsMap.own)}
+              noSubPath={true}
+              title="Your Projects" />
+          </NavItem>
+          <NavItem>
+            <RenkuNavLink exact={false} to={getPreciseUrl(sectionsMap.starred)} title="Starred Projects" />
+          </NavItem>
+          <NavItem>
+            <RenkuNavLink exact={false} to={getPreciseUrl(sectionsMap.all)} title="All Projects" />
+          </NavItem>
+        </Nav>
+      </Col>
+    </Row>
+  );
+}
+
+// TODO: Remove NEW from the function name
+function ProjectListNew(props) {
+  const {
+    fetched, fetching, getAvatar, logged, orderByMap, params, projects, search, searchInMap, sectionsMap,
+    setTarget, urlMap, users, target
+  } = props;
+
+  const newProjectButton = logged ?
+    (<Link className="btn btn-primary mt-auto mb-auto" role="button" to={urlMap.projectNewUrl}>New project</Link>) :
+    null;
+  const navBar = logged ?
+    (<ProjectListNav key="navbar" urlMap={urlMap}
+        getPreciseUrl = {props.getPreciseUrl}
+        sectionsMap={props.sectionsMap}
+        //goToSection={props.goToSection}
+         />) :
+    null;
+
+  return (
+    <Fragment>
+      <Row>
+        <Col className="d-flex mb-2">
+          <h1 className="mr-4">Projects</h1>
+          {newProjectButton}
+        </Col>
+      </Row>
+      {navBar}
+      <ProjectListContent
+        fetching={fetching}
+        fetched={fetched}
+        getAvatar={getAvatar}
+        orderByMap={orderByMap}
+        params={params}
+        projects={projects}
+        search={search}
+        searchInMap={searchInMap}
+        sectionsMap={sectionsMap}
+        setTarget={setTarget}
+        users={users}
+        target={target}
+      />
+    </Fragment>
+  );
+}
+
+
 export default ProjectList;
-export { ProjectListRow };
+export { ProjectListRow, ProjectListNew };
